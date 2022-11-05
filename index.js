c7260c85e243452cfbfe8d9f05b492c865c70069
Ewerton Ribeiro da Silva <ewerton.silvatv96@gmail.com>
2022-11-05 16:00:46 -0300

    home route systems

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-const router = require('./router/router');
-app.use('/', router);
-
-
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.set(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'))
+app.set('/views', path.join(__dirname, 'views'))
 
 
+// const router = require('./src/router/router');
+// app.use('/', router);
 
-app.listen(3000, ()=>{
+app.get('/', (req, res)=>{
+    res.render('pages/home');
+})
+
+app.listen(port, ()=>{
     console.log('o servidor está rodando');
 })
