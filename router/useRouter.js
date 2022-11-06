1b58de624a9d3620d01c78ff2b60b9f5056fb962
Ewerton Ribeiro da Silva <ewerton.silvatv96@gmail.com>
2022-11-06 01:35:29 -0300

    add label to forms

--- router/useRouter.js
+++ router/useRouter.js
@@ -5,4 +5,16 @@ router.get('/', (req, res)=>{
     res.render('pages/home');
 })
 
+
+router.get('/login', (req, res)=>{
+    res.render('pages/login');
+});
+
+router.get('/signin', (req, res)=>{
+    res.render('pages/signin');
+})
+
+router.post('/signin', (req, res)=>{  
+    res.send('cadastrado com sucesso!');
+})
 module.exports = router;
